665a021ab6487452c81623519d421e1f7dae6261
fredcaldrisc <fredcaldrisc@gmail.com>
2026-09-10 15:38:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,6 @@ def incoming():
 def memory():
     return open(FILE).read()
 
-if _name_ == "_main_":
+if __name__ == "_main_":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
